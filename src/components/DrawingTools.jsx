10b6d58b2c9ffb38906c8002dcc4051f59488e26
Asin-Junior-Honore <asinhonore823@gmail.com>
2025-01-30 10:37:40 +0100

    feat: added new cta buttons

--- src/components/DrawingTools.jsx
+++ src/components/DrawingTools.jsx
@@ -55,9 +55,21 @@ const DrawingTools = ({
                 type="text"
                 value={text}
                 onChange={(e) => setText(e.target.value)}
-                className="w-full px-4 py-2 border rounded-lg shadow focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="lg:w-[50%] px-4 py-2 border border-green-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="Enter text"
               />
+              <button
+                onClick={(e) => setText("")}
+                className="px-4 py-2 bg-blue-500 mx-8 text-white rounded-lg shadow hover:bg-blue-700 transition"
+              >
+                Clear Text Input
+              </button>
+              <button
+                onClick={() => handleToolClick("")}
+                className="px-4 py-2 bg-red-500 text-white rounded-lg shadow hover:bg-red-600 transition"
+              >
+                Close Text Input
+              </button>
             </div>
           )}
         </>
